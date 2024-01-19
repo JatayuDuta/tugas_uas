254d3ae1f6ba2ee60753d4e358449a416640caca
JatayuDuta <150224859+JatayuDuta@users.noreply.github.com>
2024-01-19 13:47:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// smooth scroll
+// animasi smooth scroll
+$('.page-scroll').on('click', function(e) {
+
+    var tujuan = $(this).attr('href');
+   
+    var elemenTujuan = $(tujuan);
+   
+    $('html , body').animate({
+     scrollTop: elemenTujuan.offset().top - 50
+    },1000);
+   
+    e.preventDefault();
+   });
